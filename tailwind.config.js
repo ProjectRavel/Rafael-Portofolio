200403ffa33d2f68edaa1cabd581955a153ad231
ProjectRavel <rafaelcodeid@gmail.com>
2024-06-06 01:07:21 +0700

    Menambahkan Source Code Portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#64748b',
+        dark: '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+}
