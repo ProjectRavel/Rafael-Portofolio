4012e6d4a809dd4b68cd62c5f8bf31930f0d44e6
ProjectRavel <rafaelcodeid@gmail.com>
2024-06-07 21:41:46 +0700

    Menambahkan DarkMode pada Website

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,9 +3,18 @@
 const hamburger = document.querySelector('#hamburger');
 const navmenu = document.querySelector('#navmenu')
 
+
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navmenu.classList.toggle('hidden')
+})
+
+// Hamburger Click
+window.addEventListener('click', function(i){
+    if(i.target != navmenu && i.target != hamburger){
+        hamburger.classList.remove('hamburger-active')
+        navmenu.classList.add('hidden')
+    }
 })
 
 // Navbar Fixed
@@ -48,3 +57,35 @@ document.getElementById('see-more').addEventListener('click', function() {
                 this.textContent = 'See Less';
             }
         });
+
+
+    function darkfunction(){
+        const darktoggle = document.querySelector('#dark-toggle')
+        const toggledark = document.querySelector('#togglebutton')
+        const html = document.querySelector('html')
+
+        
+
+        if(darktoggle.checked == true){
+            toggledark.classList.add('toggle-circle')
+            html.classList.add('dark')
+            localStorage.theme = 'dark'
+        }else{
+            toggledark.classList.remove('toggle-circle')
+            html.classList.remove('dark')
+            localStorage.theme = 'light '
+        }
+
+        
+    }
+
+    const darktoggle = document.querySelector('#dark-toggle')
+    const toggledark = document.querySelector('#togglebutton')
+
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        darktoggle.checked = true
+        toggledark.classList.add('toggle-circle')
+    } else {
+        toggledark.classList.remove('toggle-circle')
+        darktoggle.checked = false
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -10,7 +11,7 @@ module.exports = {
       colors: {
         primary: '#0ea5e9',
         secondary: '#64748b',
-        dark: '#1e293b',
+        dark: '#0f172a',
       },
     },
   },
